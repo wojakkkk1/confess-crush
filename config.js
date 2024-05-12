c7acc37657e116bf80c08ed7b85a781a104775c3
wojakkkk1 <ibadmemmedzade1@gmail.com>
2024-05-12 17:59:39 +0400

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,10 @@ const CONFIG = {
     desc: 'Bu altıncı gün kinoya baxmağa nə diyərsən?',
     btnYes: 'Əlllləbdə ki, haçan istəsən',
     btnNo: 'yx',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'İstəyicəyini bilirdimmm',
+    btnReply: 'İndi isə bura fikir vermədən TG da mənə nə giyinəcəyini ee yəni nəyə baxacağımızı di',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+    }
